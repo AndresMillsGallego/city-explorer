6980e6f87050821d6e7544e6c635124414213267
Andres Mills Gallego <geneseewoodworking@gmail.com>
2022-02-18 11:04:53 -0800

    followed along with Ryan 02/18

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Main extends React.Component {
+  render() {
+    return (
+      <main>
+        <p>This is my main for now, thanks for all the fish.</p>
+      </main>
+    )
+  }
+};
+
+export default Main;
