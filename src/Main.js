6dfef5330b4e283a261cd7043be79c068b51bcae
Andres Mills Gallego <geneseewoodworking@gmail.com>
2022-02-21 23:30:05 -0800

    added zoom effect and touched up CSS

--- src/Main.js
+++ src/Main.js
@@ -3,6 +3,16 @@ import React from 'react'
 import './Main.css'
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      zoom: 14
+    }
+  }
+
+  zoomOut = () => this.setState({zoom: this.state.zoom - 1});
+
+  zoomIn = () => this.setState({zoom: this.state.zoom + 1});
 
   render() {
     let cityName;
@@ -14,7 +24,7 @@ class Main extends React.Component {
       cityName = this.props.cityData[0].display_name;
       cityLat = this.props.cityData[0].lat;
       cityLon = this.props.cityData[0].lon;
-      cityMap = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ}&center=${cityLat},${cityLon}&zoom=13`
+      cityMap = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ}&center=${cityLat},${cityLon}&zoom=${this.state.zoom}`
     }
 
     return (
@@ -25,7 +35,10 @@ class Main extends React.Component {
             <p>The location you have selected is <b id='name'>{cityName}</b></p>
             <p>The latitude for your selected city is: <b id='lat'>{cityLat}</b></p>
             <p>The longitude for your selected city is: <b id='lon'>{cityLon}</b></p>
-            <img src={cityMap} alt={cityName} title={cityName}/>
+            <div id='zoomDiv'>
+            <button onClick={this.zoomOut}>- Zoom Out -</button> <button onClick={this.zoomIn}>+ Zoom In +</button>
+            </div>
+            <img src={cityMap} alt={cityName} title={cityName} className="h-25 w-25"/>
           </article>
         }
 
