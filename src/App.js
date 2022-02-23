d17338826daa4b21c6c3a1d973cf8f6df7ac3a3c
Andres Mills Gallego <geneseewoodworking@gmail.com>
2022-02-23 13:32:04 -0800

    got the elements bootstrapped and got weather to render

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Footer from './Footer';
 import CityForm from './CityForm'
 import Weather from './Weather'
+import Row from 'react-bootstrap/Row'
 
 class App extends React.Component {
   constructor(props) {
@@ -16,35 +17,44 @@ class App extends React.Component {
   };
 
   getCityData = (cityData) => {
-    this.setState({cityData: cityData});
+    this.setState({ cityData: cityData });
+    console.log(this.state.cityData[0].display_name)
   }
-  
+
   getCityWeather = (cityWeather) => {
-    this.setState({cityWeather: cityWeather});
-    console.log(this.state.cityWeather[0].date)
+    this.setState({ cityWeather: cityWeather });
+    console.log(this.state.cityWeather[0])
   }
-  
+
   render() {
-    
+    let weatherCards = this.state.cityWeather.map((day, index)  => (
+      <Weather key={index}
+        cityData={this.state.cityData}
+        cityWeather={this.state.cityWeather}
+      ></Weather>
+    ));
+
     return (
       <>
+
         <Header />
-        <CityForm 
-        getCityData={this.getCityData}
-        getCityWeather={this.getCityWeather}
+        <CityForm
+          getCityData={this.getCityData}
+          getCityWeather={this.getCityWeather}
         />
-        <Main 
-        cityData={this.state.cityData}
-        selectedCity={this.state.selectedCity}
-        />
-        { this.state.cityWeather.length > 0 &&
-        <p>{this.state.cityWeather[0].date}</p>
-        }
-        <Weather
+        <Main
           cityData={this.state.cityData}
-          cityWeather={this.state.cityWeather}
-        ></Weather>
+          selectedCity={this.state.selectedCity}
+        />
+        {this.state.cityWeather.length > 0 &&
+          // <p>{this.state.cityWeather[0].date}</p>
+          <Row xs={1} md={2} lg={3}>
+            {weatherCards}
+          </Row>
+        }
+        
         <Footer />
+
       </>
     )
   }
