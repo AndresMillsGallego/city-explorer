234aa6f12964a9c33ad876996fd5e229020b5d04
Andres Mills Gallego <geneseewoodworking@gmail.com>
2022-02-25 20:47:00 -0800

    switched back to live server

--- src/CityForm.js
+++ src/CityForm.js
@@ -46,7 +46,7 @@ class CityForm extends React.Component {
   
   getWeather = async () => {
     try {
-      let weatherUrl = `http://localhost:3001/weather?lat=${this.state.cityData.lat}&lon=${this.state.cityData.lon}`
+      let weatherUrl = `https://city-explorer-206.herokuapp.com/weather?lat=${this.state.cityData.lat}&lon=${this.state.cityData.lon}`
       let cityWeather = await axios.get(weatherUrl);
       this.props.getCityWeather(cityWeather.data);
     } catch (error) {
